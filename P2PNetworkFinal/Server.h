2d0c5fa2d1cede6c225f7c68cf3620d4ee4f9ea8
treardon17 <tdr.reardon@gmail.com>
2015-12-07 13:55:38 -0800

    get_driver_instance() gives unresolved externals error

--- P2PNetworkFinal/Server.h
+++ P2PNetworkFinal/Server.h
@@ -113,40 +113,47 @@ public:
 	static void serverListen(Socket* conn, Server* server) {
 
 		//ADD MYSQL DATABASE CONNECTION HERE
+		
 		try {
 			sql::Driver *driver;
 			sql::Connection *con;
 			sql::Statement *stmt;
 			sql::ResultSet *res;
 
-			/* Create a connection */
+			
+			// Create a connection
 			driver = get_driver_instance();
+			/*
 			con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
-			/* Connect to the MySQL test database */
+			// Connect to the MySQL test database
 			con->setSchema("test");
 
 			stmt = con->createStatement();
 			res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
 			while (res->next()) {
 				std::cout << "\t... MySQL replies: ";
-				/* Access column data by alias or column name */
+				// Access column data by alias or column name
 				std::cout << res->getString("_message") << std::endl;
 				std::cout << "\t... MySQL says it again: ";
-				/* Access column fata by numeric offset, 1 is the first column */
+				// Access column fata by numeric offset, 1 is the first column
 				std::cout << res->getString(1) << std::endl;
 			}
 			delete res;
 			delete stmt;
 			delete con;
+			*/
 
 		}
 		catch (sql::SQLException &e) {
+			/*
 			std::cout << "# ERR: SQLException in " << __FILE__;
 			std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
 			std::cout << "# ERR: " << e.what();
 			std::cout << " (MySQL error code: " << e.getErrorCode();
 			std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+			*/
 		}
+		
 		//END MYSQL INSERT
 
 		std::string msg;
