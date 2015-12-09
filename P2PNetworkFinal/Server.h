741ec295c16739debe0ebf34ed8f235b12e9f7d1
treardon17 <tdr.reardon@gmail.com>
2015-12-08 20:29:44 -0800

    Removing MySQL

--- P2PNetworkFinal/Server.h
+++ P2PNetworkFinal/Server.h
@@ -12,10 +12,7 @@
 #include "Socket.h"
 
 //DATABASE CONNECTION LIBRARIES
-#include "resultset.h"
-#include "driver.h"
-#include "exception.h"
-#include "statement.h"
+
 //END DATABASE CONNECTION
 
 class Server {
@@ -114,45 +111,7 @@ public:
 
 		//ADD MYSQL DATABASE CONNECTION HERE
 		
-		try {
-			sql::Driver *driver;
-			sql::Connection *con;
-			sql::Statement *stmt;
-			sql::ResultSet *res;
-
-			
-			// Create a connection
-			driver = get_driver_instance();
-			/*
-			con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
-			// Connect to the MySQL test database
-			con->setSchema("test");
-
-			stmt = con->createStatement();
-			res = stmt->executeQuery("SELECT 'Hello World!' AS _message");
-			while (res->next()) {
-				std::cout << "\t... MySQL replies: ";
-				// Access column data by alias or column name
-				std::cout << res->getString("_message") << std::endl;
-				std::cout << "\t... MySQL says it again: ";
-				// Access column fata by numeric offset, 1 is the first column
-				std::cout << res->getString(1) << std::endl;
-			}
-			delete res;
-			delete stmt;
-			delete con;
-			*/
-
-		}
-		catch (sql::SQLException &e) {
-			/*
-			std::cout << "# ERR: SQLException in " << __FILE__;
-			std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
-			std::cout << "# ERR: " << e.what();
-			std::cout << " (MySQL error code: " << e.getErrorCode();
-			std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
-			*/
-		}
+		
 		
 		//END MYSQL INSERT
 
